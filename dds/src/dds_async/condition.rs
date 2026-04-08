0d915753b5c914ed0f6d34265ac9c7f1b942c20d
Joao Rebelo <2730147+jrebelo@users.noreply.github.com>
2026-04-08 17:36:04 +0200

    Combine status condition channel with dcps_sender (#525)
    * Replace bound by option to allow static initialization
    
    * Replace name from string to static str
    
    * Replace label from Vec to Option
    
    * Replace clear_nonkey_members by local function
    
    * Remove DynamicType from DynamicData
    
    * Fix clippy warnings
    
    * Replace Vec from DynamicType by static reference to allow static building
    
    * Format code
    
    * Use static objects instead of factory for primitive types
    
    * Add get_custom_instance function
    
    * Add Default for the std runtime
    
    * Remove call to get_type on reception of builtin topics
    
    * Fix clippy
    
    * Make the dynamic type static
    
    * Delete commented code
    
    * Adjust type support creation to use statics
    
    * Replace Arc by static reference to dynamictype
    
    * Fix interoperability tests
    
    * Fix clippy
    
    * Fix formatting
    
    * Fix wrong type for discovery LivelinessQosPolicy type
    
    * Allow the PID_PARTICIPANT_GUID to have a default value
    
    * Fix wrong PID
    
    * Replace DynamicType by trait to allow removing the memory leak on the KeyHolder
    
    * Fix clippy
    
    * Use dcps sender for status condition communication
    
    * Use common channel for status condition
    
    * Fix clippy
    
    * Fix no_std tests
    
    * Fix wrong condition handling

--- dds/src/dds_async/condition.rs
+++ dds/src/dds_async/condition.rs
@@ -1,9 +1,11 @@
 use crate::{
     dcps::{
-        actor::ActorAddress,
-        channels::{mpsc::MpscReceiver, oneshot::oneshot},
-        status_condition::DcpsStatusCondition,
-        status_condition_mail::DcpsStatusConditionMail,
+        channels::{
+            mpsc::{MpscReceiver, MpscSender},
+            oneshot::oneshot,
+        },
+        dcps_mail::{DcpsMail, StatusConditionMail},
+        status_condition::StatusConditionEntity,
     },
     infrastructure::{error::DdsResult, status::StatusKind},
 };
@@ -11,28 +13,38 @@ use alloc::vec::Vec;
 
 /// Async version of [`StatusCondition`](crate::infrastructure::condition::StatusCondition).
 pub struct StatusConditionAsync {
-    address: ActorAddress<DcpsStatusCondition>,
+    dcps_sender: MpscSender<DcpsMail>,
+    entity: StatusConditionEntity,
 }
 
 impl Clone for StatusConditionAsync {
     fn clone(&self) -> Self {
         Self {
-            address: self.address.clone(),
+            dcps_sender: self.dcps_sender.clone(),
+            entity: self.entity.clone(),
         }
     }
 }
 
 impl StatusConditionAsync {
-    pub(crate) fn new(address: ActorAddress<DcpsStatusCondition>) -> Self {
-        Self { address }
+    pub(crate) fn new(dcps_sender: MpscSender<DcpsMail>, entity: StatusConditionEntity) -> Self {
+        Self {
+            dcps_sender,
+            entity,
+        }
     }
 
     pub(crate) async fn register_notification(&self) -> DdsResult<MpscReceiver<()>> {
         let (reply_sender, reply_receiver) = oneshot();
-        self.address
-            .send_actor_mail(DcpsStatusConditionMail::RegisterNotification { reply_sender })
+        self.dcps_sender
+            .send(DcpsMail::StatusCondition(
+                StatusConditionMail::RegisterNotification {
+                    entity: self.entity.clone(),
+                    reply_sender,
+                },
+            ))
             .await?;
-        reply_receiver.await
+        reply_receiver.await?
     }
 }
 
@@ -41,23 +53,31 @@ impl StatusConditionAsync {
     #[tracing::instrument(skip(self))]
     pub async fn get_enabled_statuses(&self) -> DdsResult<Vec<StatusKind>> {
         let (reply_sender, reply_receiver) = oneshot();
-        self.address
-            .send_actor_mail(DcpsStatusConditionMail::GetStatusConditionEnabledStatuses {
-                reply_sender,
-            })
+        self.dcps_sender
+            .send(DcpsMail::StatusCondition(
+                StatusConditionMail::GetStatusConditionEnabledStatuses {
+                    entity: self.entity.clone(),
+                    reply_sender,
+                },
+            ))
             .await?;
-        reply_receiver.await
+        reply_receiver.await?
     }
 
     /// Async version of [`set_enabled_statuses`](crate::infrastructure::condition::StatusCondition::set_enabled_statuses).
     #[tracing::instrument(skip(self))]
     pub async fn set_enabled_statuses(&self, mask: &[StatusKind]) -> DdsResult<()> {
-        self.address
-            .send_actor_mail(DcpsStatusConditionMail::SetStatusConditionEnabledStatuses {
-                status_mask: mask.to_vec(),
-            })
+        let (reply_sender, reply_receiver) = oneshot();
+        self.dcps_sender
+            .send(DcpsMail::StatusCondition(
+                StatusConditionMail::SetStatusConditionEnabledStatuses {
+                    entity: self.entity.clone(),
+                    status_mask: mask.to_vec(),
+                    reply_sender,
+                },
+            ))
             .await?;
-        Ok(())
+        reply_receiver.await?
     }
 
     /// Async version of [`get_entity`](crate::infrastructure::condition::StatusCondition::get_entity).
@@ -72,11 +92,14 @@ impl StatusConditionAsync {
     #[tracing::instrument(skip(self))]
     pub async fn get_trigger_value(&self) -> DdsResult<bool> {
         let (reply_sender, reply_receiver) = oneshot();
-        self.address
-            .send_actor_mail(DcpsStatusConditionMail::GetStatusConditionTriggerValue {
-                reply_sender,
-            })
+        self.dcps_sender
+            .send(DcpsMail::StatusCondition(
+                StatusConditionMail::GetStatusConditionTriggerValue {
+                    entity: self.entity.clone(),
+                    reply_sender,
+                },
+            ))
             .await?;
-        reply_receiver.await
+        reply_receiver.await?
     }
 }
